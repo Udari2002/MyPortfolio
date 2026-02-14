6dcab592f554b8c43612aeab572bd4cae7f2ddc3
Moksha <eg245236@engug.ruh.ac.lk>
2026-02-15 01:02:57 +0530

    Add shadow customization and update About section details

--- components/About.jsx
+++ components/About.jsx
@@ -8,9 +8,9 @@ const About = () => {
         <h4 className="text-center mb-2 text-lg font-Ovo">Introduction</h4>
         <h2 className="text-center text-5xl font-Ovo">About Me</h2>
         <div className="flex w-full flex-col lg:flex-row items-center gap-20 my-20">
-            <div className="w-64 sm:w-80 rounded-3xl max-w-none">
+            <div className="w-56 sm:w-64 rounded-3xl max-w-none">
                 <Image src={assets.user_image} alt="User Image" 
-                className="w-full rounded-3xl" />
+                className="w-full rounded-3xl aspect-square object-cover" />
             </div>
             <div className="flex-1">
                 <p className="mb-10 max-w-2xl font-Ovo">I am an experienced Frontend Devoloper with over
@@ -22,7 +22,7 @@ const About = () => {
                     {infoList.map(({icon,iconDark, title, description},index) => (
                         <li className="border-[0.5px] border-gray-400
                          rounded-xl p-6 cursor-pointer hover:bg-lightHover
-                         hover:-translate-y-1 duration-500" 
+                         hover:-translate-y-1 duration-500 hover:shadow-black" 
                         key={index}>
                             <Image src={icon} alt={title} className="w-7 mt-3" />
                             <h3 className="my-4 font-semibold
