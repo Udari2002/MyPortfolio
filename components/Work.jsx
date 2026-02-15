208c13e1fdbb919f0eabbecd9795958eb621a251
Moksha <eg245236@engug.ruh.ac.lk>
2026-02-15 23:02:13 +0530

    Add Work component to showcase recent projects with responsive grid layout

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,4 +1,6 @@
+import { assets, workData } from '@/assets/assets'
 import React from 'react'
+import Image from 'next/image'
 
 const Work = () => {
   return (
@@ -8,6 +10,26 @@ const Work = () => {
         <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
             Here are some of my recent projects that showcase my skills and expertise in web development.
         </p>
+
+        <div className="grid grid-cols-auto my-10 gap-5">
+            {workData.map((project,index) => (
+                <div key={index}
+                className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative
+                 cursor-pointer group'
+                style={{backgroundImage: `url(${project.bgImage})`}}>
+                    <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2
+                    -translate-x-1/2 py-5 px-3 flex items-center justify-between'>
+                      <div>
+                        <h2>{project.title}</h2>
+                        <p>{project.description}</p>
+                      </div>
+                    </div>
+                    <div>
+                        <Image src={assets.send_icon} alt='send icon' className="w-5"/>
+                    </div>
+                 </div>
+            ))}
+        </div>
     </div>
   )
 }
